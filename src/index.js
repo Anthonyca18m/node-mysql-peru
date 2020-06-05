159fd79fe3f095e1159a997693e39786cea86091
anthonyca18m <anthonyca18m@gmail.com>
2020-06-05 16:33:37 -0500

    v4

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 
 //Routes
 // app.use(require('./routes'));
-app.use('/api', routes);
+app.use(routes);
 
 //Public 
 // app.use(express.static(path.join(__dirname, 'public')));
