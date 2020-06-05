145b0e40925253b21ef907e4dfd02652fea36a1d
anthonyca18m <anthonyca18m@gmail.com>
2020-06-05 15:56:17 -0500

    Config of db in heroku

--- src/Config/database.js
+++ src/Config/database.js
@@ -1,11 +1,11 @@
 const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize(
-  'node_mysql_peru',
-  'root',
-  '',
+  'heroku_c7c7399b6f0aa7c',
+  'baf7b6582c4985',
+  '7c426a19',
   {
-    host: 'localhost',
+    host: 'us-cdbr-east-05.cleardb.net',
     dialect: 'mysql'
   }
 );
